f6227dade6eac0d86efc5727e3d1aa370dd3e9e0
littleicecream <78819905+littleicecream@users.noreply.github.com>
2021-08-03 20:41:12 +0600

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://trackerslist.com/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_exclude.txt | awk '$0' | tr '\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=10
+export MAX_CONCURRENT_DOWNLOADS=5
 
 aria2c --enable-rpc --rpc-listen-all=false --check-certificate=false \
    --max-connection-per-server=16 --rpc-max-request-size=1024M \
@@ -11,5 +11,5 @@ aria2c --enable-rpc --rpc-listen-all=false --check-certificate=false \
    --peer-id-prefix=-qB4360- --user-agent=qBittorrent/4.3.6 --peer-agent=qBittorrent/4.3.6 \
    --disk-cache=256M --file-allocation=prealloc --continue=true \
    --max-file-not-found=5 --max-tries=20 --auto-file-renaming=true \
-   --seed-time=0.01 --seed-ratio=1.0 \
+   --seed-time=0.01 --seed-ratio=1.0 --bt-enable-lpd=true --enable-dht=true --enable-dht6=true --enable-peer-exchange=true \
    --content-disposition-default-utf8=true --http-accept-gzip=true --reuse-uri=true --netrc-path=/usr/src/app/.netrc
